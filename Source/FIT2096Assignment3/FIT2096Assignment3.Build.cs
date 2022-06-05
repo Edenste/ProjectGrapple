e2c3e6165f41e431dcd91f105e55c06ee6fcd435
Steven Pham <edensteven@protonmail.com>
2022-06-05 20:39:05 +1000

    Enabled Niagra in C++

--- Source/FIT2096Assignment3/FIT2096Assignment3.Build.cs
+++ Source/FIT2096Assignment3/FIT2096Assignment3.Build.cs
@@ -8,6 +8,6 @@ public class FIT2096Assignment3 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Niagara" });
 	}
 }
